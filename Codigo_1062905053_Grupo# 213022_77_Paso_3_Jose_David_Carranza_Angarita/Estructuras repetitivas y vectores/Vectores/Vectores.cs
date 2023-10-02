2bc2a438d9d450de801a73d632e3d0c64119952b
JOSE DAVID CARRANZA ANGARITA <jodacan2005@hotmail.com>
2023-10-02 18:36:08 -0500

    Add files via upload

---
+++ Codigo_1062905053_Grupo# 213022_77_Paso_3_Jose_David_Carranza_Angarita/Estructuras repetitivas y vectores/Vectores/Vectores.cs
@@ -0,0 +1,58 @@
+﻿
+/*Almacenar en un vector 8 números enteros positivos entre 3 y 6,
+ambos incluidos, en caso de no cumplir se debe repetir la solicitud
+hasta que se cumpla con la condición. Almacenar en un vector de 4
+posiciones la cantidad de veces que se repite cada el 3, 4, 5 y 6. Al
+final mostrar el listado de los 8 números ingresados, la cantidad de
+veces que se repitió cada número y el valor de cada uno elevado a
+la cantidad de veces que se repitió.*/
+
+/*
+* Nombre: Jose David Carranza Angarita
+* Grupo : 213022_77
+* Programa : Ingeniería de Sistemas 
+* Fuente: autoria propia
+* Fecha : 02/09/2023
+*/
+
+
+using System;
+
+namespace paso_3_vectores
+{
+    class Vectores
+    {
+        static void Main()
+        {
+            Console.Title = " Estructuras Repetitivas y Vectores";
+
+            int[] numeros = new int[8];
+            int[] conteo = new int[4];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                do
+                {
+                    Console.Write("Ingrese un número entero positivo entre 3 y 6 : " + (i + 1) + ": ");
+                    numeros[i] = Convert.ToInt32(Console.ReadLine());
+                } while (numeros[i] < 3 || numeros[i] > 6);
+
+                conteo[numeros[i] - 3]++;
+            }
+
+            Console.WriteLine("\nNúmeros ingresados:");
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Posición " + (i + 1) + ": " + numeros[i]);
+            }
+
+            Console.WriteLine("\nConteo de repeticiones y valor elevado a la cantidad de veces que se repitió:");
+            for (int i = 0; i < conteo.Length; i++)
+            {
+                Console.WriteLine("Número " + (i + 3) + " se repitió " + conteo[i] + " veces. Valor elevado a la cantidad de veces que se repitió: " + Math.Pow(i + 3, conteo[i]));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Codigo_1062905053_Grupo# 213022_77_Paso_3_Jose_David_Carranza_Angarita/Matrices Bidimensionales/Matrices/Matrices.cs
@@ -0,0 +1,85 @@
+﻿/*La La liga de atletismo Corriendo Ligero tiene 6 corredores de 100
+metros planos de los cuales necesita clasificar mínimo 2 para las
+finales del torneo nacional de atletismo. Almacenar en un vector de
+tipo string el nombre de cada atleta y en una matriz de 6X6, tipo
+entero, los tiempos de cada carrera realizada, recuerde que cada fila
+corresponde a los tiempos de un corredor en las 6 carreras y cada
+columna corresponde a los tiempos en cada carrera.
+Para la clasificación se usará dos criterios, el primero será el mejor
+registro (el menor tiempo) que se ha obtenido en una carrera y el
+segundo será el mejor promedio por carrera. Sí alguno de los
+tiempos es mayor a 10.4 segundos el atleta queda descalificado. Al
+final mostrar por pantalla toda la información de los atletas
+3
+(nombres y tiempos por carrera) y los nombres de los clasificados
+con sus tiempos, en caso de empate se mostrarán todos los nombres
+y en caso de descalificación mostrar un mensaje que incluya el
+tiempo.
+*/
+
+/*
+* Nombre: Jose David Carranza Angarita
+* Grupo : 213022_77
+* Programa : Ingeniería de Sistemas 
+* Fuente: autoria propia
+* Fecha : 02/09/2023
+*/
+
+
+using System;
+
+namespace Paso_3_Matrices
+{
+    class Matrices
+    {
+        static void Main()
+        {
+            Console.Title = "Matrices Bidimencionales";
+
+            string[] nombres = new string[6];
+            double[,] tiempos = new double[6, 6];
+            double[] promedios = new double[6];
+            double[] mejoresTiempos = new double[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Ingrese el nombre del atleta " + (i + 1) + ": ");
+                nombres[i] = Console.ReadLine();
+
+                double mejorTiempo = Double.MaxValue;
+                double sumaTiempos = 0;
+
+                for (int j = 0; j < 6; j++)
+                {
+                    Console.Write("Ingrese el tiempo de la carrera " + (j + 1) + " del atleta " + nombres[i] + ": ");
+                    tiempos[i, j] = Double.Parse(Console.ReadLine());
+
+                    if (tiempos[i, j] > 10.4)
+                    {
+                        Console.WriteLine("El atleta " + nombres[i] + " ha sido descalificado por tener un tiempo mayor a 10.4 segundos.");
+                        return;
+                    }
+
+                    if (tiempos[i, j] < mejorTiempo)
+                    {
+                        mejorTiempo = tiempos[i, j];
+                    }
+
+                    sumaTiempos += tiempos[i, j];
+                }
+
+                mejoresTiempos[i] = mejorTiempo;
+                promedios[i] = sumaTiempos / 6.0;
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("\nAtleta: " + nombres[i]);
+                Console.WriteLine("Mejor tiempo: " + mejoresTiempos[i]);
+                Console.WriteLine("Promedio de tiempos: " + promedios[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
